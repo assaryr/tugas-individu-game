71b06e318ad303172bec45fbbdebabc8f22f6092
assaryr <assaryriva@gmail.com>
2019-12-13 18:16:39 +0700

    first commit

---
+++ Assets/Scripts/belajargame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class belajargame : MonoBehaviour {
+    
+	public string pesan;
+	public int kecepatan;
+	public float waktu;
+	public bool aktif;
+	
+	
+	void Start () {
+		
+	}
+
+
+
+    // Update is called once per frame
+    void Update() {
+		waktu += Time.deltaTime;
+		
+		if(waktu>3) {
+			aktif=true;
+		}
+		
+		if (aktif == true) {
+			print (pesan);
+			kecepatan=5;
+		}
+	}
+}
---
+++ Assets/Scripts/rotasi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotasi : MonoBehaviour {
+	
+	public int kecepatanX,kecepatanY,kecepatanZ;
+    // Use this for initalization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		transform.Rotate (kecepatanX,kecepatanY,kecepatanZ);
+        
+    }
+}
